dcc3b4565b42ef212e8ed7e60b1262fdf493314d
onlyedit1078-commits <onlyedit1078@gmail.com>
2026-08-04 12:22:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,13 +50,12 @@ function typeWriter() {
 }
 
 window.onload = typeWriter;
-
 const photos = [
-IMG-20260804-WA0000.jpg
-IMG-20260804-WA0002.jpg
-IMG-20260804-WA0003.jpg
-IMG-20260804-WA0005.jpg
-IMG-20260804-WA0006.jpg
+"IMG-20260804-WA0000.jpg",
+"IMG-20260804-WA0002.jpg",
+"IMG-20260804-WA0003.jpg",
+"IMG-20260804-WA0005.jpg",
+"IMG-20260804-WA0006.jpg"
 ];
 let current = 0;
 
